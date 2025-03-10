33fbcad74728cbab914a534535dca17468ae79bb
MrVorgoth <skay55@interia.pl>
2025-03-10 14:14:15 +0100

    Remove swarms lib

--- src/ai-engine/agents/agent.py
+++ src/ai-engine/agents/agent.py
@@ -1,20 +1,7 @@
-import os
 from dotenv import load_dotenv
-from swarms import Agent
-from swarm_models import OpenAIChat
 
 load_dotenv()
 
-# Initialize the language model
-llm = OpenAIChat(
-    temperature=0.1, model_name="gpt-4o-mini", max_tokens=1000
-)
-
 def run_agent(message: dict):
-  ## Initialize the workflow
-  agent = Agent(llm=llm, max_loops=1, autosave=True, dashboard=True)
-  
-  # Run the workflow on a task
-  response = agent.run(message['message'])
-  print(response)
-  return response
+  print(message)
+  return message
