a2e5c353d50ce11ed05b60fd5d418c6135a4ae4e
MrVorgoth <skay55@interia.pl>
2025-03-11 09:53:28 +0100

    Add boss agent with team

--- src/ai-engine/agents/agent.py
+++ src/ai-engine/agents/agent.py
@@ -1,18 +1,11 @@
-from typing import Iterator
 from dotenv import load_dotenv
-from agno.agent import Agent, RunResponse
-from agno.models.openai import OpenAIChat
-
-agent = Agent(
-    model=OpenAIChat(id="gpt-4o-mini"),
-    description="You are an enthusiastic news reporter with a flair for storytelling!",
-    # markdown=True
-)
+from agno.agent import RunResponse
+from .boss_agent import boss_agent
 
 load_dotenv()
 
 def run_agent(message: dict):
   print(message)
-  response: RunResponse = agent.run(message['message'])
+  response: RunResponse = boss_agent.run(message['message'])
 
   return response.content
