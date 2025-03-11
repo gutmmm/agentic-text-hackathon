076fdbea50ad64fd9ebc719049b3adaa1a5124ca
MrVorgoth <skay55@interia.pl>
2025-03-11 09:29:01 +0100

    Add agno framework and basic agent

--- src/ai-engine/agents/agent.py
+++ src/ai-engine/agents/agent.py
@@ -1,7 +1,18 @@
+from typing import Iterator
 from dotenv import load_dotenv
+from agno.agent import Agent, RunResponse
+from agno.models.openai import OpenAIChat
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"),
+    description="You are an enthusiastic news reporter with a flair for storytelling!",
+    # markdown=True
+)
 
 load_dotenv()
 
 def run_agent(message: dict):
   print(message)
-  return message
+  response: RunResponse = agent.run(message['message'])
+
+  return response.content
--- src/ai-engine/routers/router.py
+++ src/ai-engine/routers/router.py
@@ -1,19 +1,16 @@
-from pydantic import BaseModel
-
 from fastapi import APIRouter
 
 from agents.agent import run_agent
 
 router = APIRouter()
 
-class InvokeRequest(BaseModel):
+class InvokeRequest():
     message: str
 
-class InvokeResponse(BaseModel):
+class InvokeResponse():
     response: str
 
 
 @router.post("/invoke")
 def invoke_agent(message: dict):
     return run_agent(message)
-
